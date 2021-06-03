bfaac4c7b2418477acf4294880e067d2752652fc
michaelfong2017 <michaelfong2017@gmail.com>
2021-06-03 14:57:25 +0800

    setup python env ipykernel

--- python/setup.sh
+++ python/setup.sh
@@ -3,6 +3,8 @@ env_name="linux-env"
 python3 -m venv $env_name
 
 $env_name/bin/python -m pip install --upgrade pip
+$env_name/bin/python -m pip install ipykernel 
+$env_name/bin/python -m ipykernel install --user --name=grpc-proto
 $env_name/bin/pip install grpcio
 $env_name/bin/pip install grpcio-tools
 
